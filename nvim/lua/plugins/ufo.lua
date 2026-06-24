d8b5e278f2531a320f1e2329e03a0a47ab243c48
Daniel B Sherry <dbsherry890@gmail.com>
2026-06-24 10:55:27 -0500

    go keymap and windows aliases

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -4,12 +4,12 @@ return {
   opts = {},
   config = function(_, opts)
     -- Neovim fold settings
-    vim.o.foldcolumn = '1' -- show fold column
-    vim.o.foldenable = true -- enable folding
-    vim.o.foldmethod = 'expr' -- use expr-based folding
+    vim.o.foldcolumn = '1'                        -- show fold column
+    vim.o.foldenable = true                       -- enable folding
+    vim.o.foldmethod = 'expr'                     -- use expr-based folding
     vim.o.foldexpr = 'nvim_treesitter#foldexpr()' -- treesitter folds
-    vim.o.foldlevel = 99 -- huge number: folds start open
-    vim.o.foldlevelstart = 99 -- start with all folds open
+    vim.o.foldlevel = 99                          -- huge number: folds start open
+    vim.o.foldlevelstart = 99                     -- start with all folds open
 
     require('ufo').setup(opts)
 
